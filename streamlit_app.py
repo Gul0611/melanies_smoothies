ac65a82533a70c111bd9c5dbb6f5cfd09eebd1c0
Gul0611 <gulafshan@provintl.com>
2026-06-22 12:17:26 +0530

    Clean up blank lines in streamlit_app.py
    Removed unnecessary blank lines before import statements.

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,' +name_on_order+ '!',icon="✅")
 
-
-
-
 import pandas as pd
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
