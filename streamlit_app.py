8615ce32a42a36f4505bf766655eb997c1a0a543
Gul0611 <gulafshan@provintl.com>
2026-06-22 12:12:56 +0530

    Update smoothie order confirmation and display data

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,11 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,' +name_on_order+ '!',icon="✅")
+
+
+
+
+import pandas as pd
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
-st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=pd.json_normalize(smoothiefroot_response.json()), use_container_width=True)
